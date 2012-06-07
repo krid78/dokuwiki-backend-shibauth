96d7493a5618b544f26f8cc1e8ade96c2b261094
Daniel Kriesten <krid@tu-chemnitz.eu>
2012-06-07 20:11:12 +0200

    cleanup and debugging
    Cleanup:
    * whitespaces
    * indentation
    
    Debuggin:
    * save $USERINFO to server variables
    
    Signed-off-by: Daniel Kriesten <krid@tu-chemnitz.eu>

--- backend/shib.class.php
+++ backend/shib.class.php
@@ -3,10 +3,10 @@
 /*
  * Shibboleth authentization backend
  *
- * @author		Ivan Novakov <ivan.novakov@debug.cz>
- * @license		GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @copyright	Ivan Novakov 2009-02-23
- * @version		0.5.0
+ * @author        Ivan Novakov <ivan.novakov@debug.cz>
+ * @license        GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @copyright    Ivan Novakov 2009-02-23
+ * @version        0.5.0
  *
  */
 
@@ -18,7 +18,7 @@ define('DEF_SHIB_HANDLEBASE', '/Shibboleth.sso');
 
 class auth_shib extends auth_basic
 {
-    
+
     var $_options = array();
     var $_userInfo = array();
 
@@ -27,7 +27,7 @@ class auth_shib extends auth_basic
     {
         $this->cando['external'] = true;
         $this->cando['logoff'] = true;
-        
+
         $this->_init();
     }
 
@@ -35,27 +35,27 @@ class auth_shib extends auth_basic
     function _init ()
     {
         global $conf;
-        
+
         $defaults = array(
-            
-            'shibboleth_base_handle' => DEF_SHIB_HANDLEBASE, 
-            'lazy_sessions' => false, 
-            'logout_redirect' => DEF_SHIB_HANDLEBASE . '/Logout?return=' . $_SERVER['HTTP_REFERER'], 
-            'var_remote_user' => 'REMOTE_USER', 
-            'var_name' => '', 
-            'var_mail' => '', 
-            'var_entitlement' => '', 
-            'var_groups' => '', 
-            'tpl_user_name' => '', 
-            'superusers' => array(), 
-            'defaultgroup' => $conf['defaultgroup'], 
-            'admingroup' => 'admin', 
-            'customgroups' => false, 
-            'customgroups_file' => DOKU_CONF . 'custom_groups.php', 
-            'entitlement_groups' => array(), 
+
+            'shibboleth_base_handle' => DEF_SHIB_HANDLEBASE,
+            'lazy_sessions' => false,
+            'logout_redirect' => DEF_SHIB_HANDLEBASE . '/Logout?return=' . $_SERVER['HTTP_REFERER'],
+            'var_remote_user' => 'REMOTE_USER',
+            'var_name' => '',
+            'var_mail' => '',
+            'var_entitlement' => '',
+            'var_groups' => '',
+            'tpl_user_name' => '',
+            'superusers' => array(),
+            'defaultgroup' => $conf['defaultgroup'],
+            'admingroup' => 'admin',
+            'customgroups' => false,
+            'customgroups_file' => DOKU_CONF . 'custom_groups.php',
+            'entitlement_groups' => array(),
             'debug' => false
         );
-        
+
         $this->_options = (array) $conf['auth']['shib'] + $defaults;
     }
 
@@ -85,44 +85,44 @@ class auth_shib extends auth_basic
 
     function _authenticate ()
     {
-        
+
         $remoteUser = $this->_getShibVar($this->_getOption('var_remote_user'));
         if ($remoteUser) {
-            
+
             $userId = $remoteUser;
             $userName = $this->_getUserName($userId);
-            
+
             $this->_userInfo = array(
-                
-            'uid' => $userId, 'name' => $userName, 'mail' => ''
+
+                'uid' => $userId, 'name' => $userName, 'mail' => ''
             );
-            
+
             $mails = $this->_getShibVar($this->_getOption('var_mail'), true);
             if (count($mails)) {
                 $this->_userInfo['mail'] = $mails[0];
             }
-            
+
             if (NULL !== $this->_getOption('defaultgroup')) {
                 $this->_addUserGroup($this->_getOption('defaultgroup'));
             }
-            
+
             if ((NULL !== $this->_getOption('superusers')) && is_array($this->_getOption('superusers')) && in_array($userId, $this->_getOption('superusers'))) {
                 $this->_addUserGroup($this->_getOption('admingroup'));
             }
-            
+
             $this->_setGroups();
             $this->_setCustomGroups($userId);
             $this->_setEntitlementGroups();
-            
+
             $this->_saveUserInfo();
-            
+
             return true;
         }
-        
+
         if (! $this->_getOption('lazy_sessions')) {
             auth_logoff();
         }
-        
+
         return false;
     }
 
@@ -134,14 +134,14 @@ class auth_shib extends auth_basic
                 return $userName;
             }
         }
-        
+
         if (($varName = $this->_getOption('var_name'))) {
             $userName = $this->_getShibVar($varName);
             if ($userName) {
                 return $userName;
             }
         }
-        
+
         return $userId;
     }
 
@@ -151,7 +151,7 @@ class auth_shib extends auth_basic
         $matches = array();
         if (preg_match_all('/({([^{}]+)})/', $tplUserName, $matches)) {
             $vars = $matches[2];
-            
+
             $userName = $tplUserName;
             foreach ($vars as $var) {
                 $value = $this->_getShibVar($var);
@@ -160,10 +160,10 @@ class auth_shib extends auth_basic
                 }
                 $userName = str_replace('{' . $var . '}', $value, $userName);
             }
-            
+
             return $userName;
         }
-        
+
         return '';
     }
 
@@ -174,7 +174,7 @@ class auth_shib extends auth_basic
         if (! $varGroups) {
             return;
         }
-        
+
         $groups = $this->_getShibVar($varGroups, true);
         foreach ($groups as $groupName) {
             $this->_addUserGroup($groupName);
@@ -188,36 +188,36 @@ class auth_shib extends auth_basic
         if (! $this->_getOption('customgroups')) {
             return;
         }
-        
+
         $groupsFile = $this->_getOption('customgroups_file');
         if (! file_exists($groupsFile)) {
             $this->_log(sprintf("Non-existent custom groups file '%s'.", $groupsFile));
             return;
         }
-        
+
         $customGroups = array();
         @include $groupsFile;
-        
+
         if (! isset($customGroups)) {
             $this->_log('Custom groups variable not found.');
             return;
         }
-        
+
         if (! is_array($customGroups) || empty($customGroups)) {
             $this->_log('No custom groups specified.');
             return;
         }
-        
+
         foreach ($customGroups as $groupName => $groupMembers) {
             if (! is_array($groupMembers) || empty($groupMembers)) {
                 continue;
             }
-            
+
             if (in_array($userId, $groupMembers)) {
                 $this->_addUserGroup($groupName);
             }
         }
-    
+
     }
 
 
@@ -228,25 +228,25 @@ class auth_shib extends auth_basic
             $this->_log('entitlement variable name not set');
             return;
         }
-        
+
         $entitlement = $this->_getShibVar($entVarName, true);
         if (! $entitlement) {
             $this->_log('no entitlement values set');
             return;
         }
-        
+
         $entGroups = $this->_getOption('entitlement_groups');
         if (! $entGroups || ! is_array($entGroups)) {
             $this->_log('entitlement groups not configured');
             return;
         }
-        
+
         foreach ($entitlement as $entVal) {
             if (isset($entGroups[$entVal])) {
                 $this->_addUserGroup($entGroups[$entVal]);
             }
         }
-    
+
     }
 
 
@@ -262,16 +262,27 @@ class auth_shib extends auth_basic
     function _saveUserInfo ()
     {
         global $USERINFO;
-        
+
         $USERINFO = $this->_userInfo;
         $_SESSION[DOKU_COOKIE]['auth']['user'] = $USERINFO['uid'];
         $_SESSION[DOKU_COOKIE]['auth']['info'] = $USERINFO;
-        
-        // Despite setting the user into the session, DokuWiki still uses hard-coded REMOTE_USER variable    
+
+        // Despite setting the user into the session, DokuWiki still uses hard-coded REMOTE_USER variable
         if ($this->_getOption('var_remote_user') != 'REMOTE_USER') {
             $_SERVER['REMOTE_USER'] = $USERINFO['uid'];
         }
-        
+
+        if ($this->_getOption('debug')) {
+            foreach ($USERINFO as $tmp1 => $tmp2) {
+                if ($tmp1 != 'grps') {
+                    $_SERVER['FINAL_USER_INFO_'.$tmp1] = $tmp2;
+                }
+            }
+            foreach ($USERINFO['grps'] as $tmp1 => $tmp2) {
+                $_SERVER['FINAL_USER_GROUPS'.$tmp1] = $tmp2;
+            }
+        }
+
         $this->_log($this->_userInfo);
     }
 
@@ -281,11 +292,11 @@ class auth_shib extends auth_basic
         if (! isset($_SERVER[$varName])) {
             return NULL;
         }
-        
+
         if (! $multivalue) {
             return $_SERVER[$varName];
         }
-        
+
         $values = explode(';', $_SERVER[$varName]);
         return $values;
     }
